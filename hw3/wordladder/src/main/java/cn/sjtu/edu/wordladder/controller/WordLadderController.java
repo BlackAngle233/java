f190ee3b9a88d78f3a6377f108d679763192a628
BlackAngle233 <740448368@qq.com>
2019-04-26 13:21:32 +0800

    finish task3

---
+++ hw3/wordladder/src/main/java/cn/sjtu/edu/wordladder/controller/WordLadderController.java
@@ -0,0 +1,38 @@
+package cn.sjtu.edu.wordladder.controller;
+
+import java.io.FileNotFoundException;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import cn.sjtu.edu.wordladder.domain.WordLadder;
+
+@RestController
+public class WordLadderController {
+
+    @RequestMapping("/")
+    public ModelAndView root() {
+        ModelAndView mv = new ModelAndView("redirect:/index");
+        return mv;
+    } 
+
+    @RequestMapping("/index")
+    public ModelAndView index() {
+        ModelAndView mv = new ModelAndView("index");
+        return mv;
+    }
+
+    @RequestMapping("/wordladder")
+    public String register(HttpServletRequest request) throws FileNotFoundException {
+        String start = request.getParameter("startword");
+        String destination = request.getParameter("targetword");
+        WordLadder wordLadder = new WordLadder();
+        wordLadder.readEnglish();
+        wordLadder.inputWords(start, destination);
+        String ans = wordLadder.showAns();
+        return ans;
+    }
+}
---
+++ task/task3/README.md
@@ -0,0 +1,1 @@
+* [dockerhub地址](https://cloud.docker.com/repository/docker/blackangle233/dockertest/general)
